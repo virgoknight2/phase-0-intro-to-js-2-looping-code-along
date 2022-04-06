1b41f4fcd70390af942622d92ee773871ac6e55a
Lorelai Knutti <virgoknight2@gmail.com>
2022-04-06 08:21:19 -0400

    made it pass tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(key,type){
+    let names = [];
+  for(let i=0; i<key.length;i++){
+      names[i]=`Thank you, ${key[i]}, for the wonderful ${type} gift!`
+    }
+    console.log(names);
+    return names;
+  }
+
+
+function countDown(x){
+    for(let age=x; age>=0; age--){
+        console.log(age)
+    }
+}
